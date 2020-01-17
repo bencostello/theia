f84199c284ba9943beea7a10a22699e2f794e204
Ben Costello <bencostello@gmail.com>
2020-01-17 09:09:05 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get upgrade
+--force-yes
+sudo apt-get install software-properties-common
+apt-get install build-essential libssl-dev
+apt install python2.7
+sudo apt-get update
+sudo apt-get upgrade
+apt install python-pip
+pip install python_language_server
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
+source ~/.bashrc
+nvm install 10
+npm install -g yarn
+yarn
+yarn theia build
+yarn theia start --hostname 0.0.0.0 --port 8080
